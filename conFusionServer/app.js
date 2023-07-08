1da9d4b51507b0ad3ac4314a45295a02f8f0d311
GfPotter <quanpotterha@gmail.com>
2023-07-08 18:12:53 +0700

    test

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -1,101 +1,104 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
-var passport = require('passport');
-var mongoose = require('mongoose');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mongoose = require("mongoose");
+const Dishes = require("./models/dishes");
+var config = require("./config.js");
+const url = config.mongoUrl;
+// const url = "mongodb://localhost:27017/conFusion";
+const connect = mongoose.connect(url);
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 const dishRouter = require("./routes/dishRouter");
 const promoRouter = require("./routes/promoRouter");
 const leadersRouter = require("./routes/leaderRouter");
-const uploadRouter = require('./routes/uploadRouter');
-var Dishes = require('./models/dishes');
-var authenticate = require('./authenticate');
-var config = require('./config');
-const url = config.mongoUrl;
-const connect = mongoose.connect(url);
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 var app = express();
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
+var passport = require("passport");
+var authenticate = require("./authenticate");
+const favoriteRouter = require('./routes/favoriteRouter')
 
-// Connect to MongoDB
-connect.then((db) => {
-  console.log('Connected correctly to server');
-}, (err) => {
-  console.log(err);
-});
-app.all('*', (req, res, next) => {
-  if (req.secure) {
-    return next();
+connect.then(
+  (db) => {
+    console.log("Connected to the server");
+  },
+  (err) => {
+    console.log(err);
   }
-  else {
-    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
-  }
-});
+);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  })
+);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Authentication middleware
-// function auth(req, res, next) {
-//   console.log(req.user);
-
-//   if (!req.user) {
-//     var err = new Error('You are not authenticated!');
-//     err.status = 403;
-//     return next(err);
-//   } else {
-//     next();
-//   }
-// }
-
-// Routes
-
-app.use('/', indexRouter);
-
-// Skip authentication middleware for the login route
-app.use('/users', usersRouter);
-
-// Apply authentication middleware for all other routes
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leadersRouter);
-app.use('/imageUpload', uploadRouter);
-// Catch 404 and forward to error handler
+app.use('/favorites', favoriteRouter);
+//cookie
+// app.use(cookieParser("12345-67890"));
+app.use(auth); // Require auth middleware function
+
+function auth(req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+    var err = new Error("You are not authenticated!");
+    err.status = 403;
+    next(err);
+  } else {
+    next();
+  }
+}
+
+// function sessionAuth(req, res, next) {
+//   console.log(req.session);
+//   if (!req.session.user) {
+//     var err = new Error("You are not authenticated!");
+//     err.status = 403;
+//     return next(err);
+//   } else {
+//     if (req.session.user === "authenticated") {
+//       next();
+//     } else {
+//       var err = new Error("You are not authenticated!");
+//       err.status = 403;
+//       return next(err);
+//     }
+//   }
+// }
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
-// Error handler
+// error handler
 app.use(function (err, req, res, next) {
-  // Set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // Render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
